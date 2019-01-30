29045cf1377d184d739a03d3345e2937a59e2802
nemexben <47148398+nemexben@users.noreply.github.com>
2019-01-30 18:05:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on('ready', () => {
      console.log(`ON ${client.guilds.size} Servers '     Script By : Dark7oveRR ' `);
    console.log(`----------------`);
  console.log(`Logged in as ${client.user.tag}!`);
- client.user.setGame(`NEMEX | PREFIX $$PLAY`,"https://www.twitch.tv/dark7overr")
+ client.user.setGame(`Moon | PREFIX $$PLAY`,"https://www.twitch.tv/dark7overr")
  client.user.setStatus("dnd")
 });
 const prefix = "$$"
